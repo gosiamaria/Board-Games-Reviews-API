d98185574baeaa00135113c503b4aed39c3bec16
Gosia <gosiamaria.siwicka@gmail.com>
2021-11-01 12:11:04 +0000

    -Sets up the files and creates all tables

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,6 +1,23 @@
-const seed = (data) => {
-  const { categoryData, commentData, reviewData, userData } = data;
-  // 1. create tables
+const db = require("../");
+const format = require("pg-format");
+const { categoryData, commentData, reviewData, userData } = require("../data/development-data/index.js");
+
+
+const seed = ({ categoryData, commentData, reviewData, userData }) => {
+  //drop tables if exist: comments, reviews, users, categories
+  return db.query(`DROP TABLE IF EXISTS comments;`).then(() => {
+      return db.query(`DROP TABLE IF EXISTS reviews;`).then(() => {
+        return db.query(`DROP TABLE IF EXISTS users;`).then(() => {
+          return db.query(`DROP TABLE IF EXISTS categories`)
+        })
+      })
+    })
+    .then(() => {
+      // 💛CREATING TABLE categories 
+
+    })
+
+
   // 2. insert data
 };
 
