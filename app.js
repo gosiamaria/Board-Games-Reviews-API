b7e7080cd57ce66b9463e772b3bd8f769c1de811
Gosia <gosiamaria.siwicka@gmail.com>
2021-11-22 12:03:40 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -6,10 +6,13 @@ const {
     handle500,
     handleNotAllowedMethods
 } = require("./controllers/errors.controllers")
+const cors = require('cors');
 
-
+app.use(cors());
 const app = express();
 app.use(express.json());
+
+
 
 app.use("/api", apiRouter);
 
